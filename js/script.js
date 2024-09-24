169db02d3f231f51dc360e4f4c8ba135fec37bc0
MD Nafizul Iqram <nafizulikram3@gmail.com>
2024-09-24 20:58:47 +0600

    Complete website and logic building

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-console.log("hello this is js file from js folder");
+document.getElementById("btn-history").addEventListener("click", function(){
+    document.getElementById("card-section").classList.add("hidden");
+    document.getElementById("history-section").classList.remove("hidden");
+    document.getElementById("btn-card").classList.add("bg-transparent");
+    document.getElementById("btn-card").classList.add("border-2");
+    document.getElementById("btn-history").classList.add("bg-lime-400")
+    document.getElementById("btn-card").classList.remove("bg-lime-400")
+    document.getElementById("btn-card").classList.add("text-slate-500")
+
+})
+
+
+document.getElementById("btn-card").addEventListener("click", function(){
+    document.getElementById("card-section").classList.remove("hidden");
+    document.getElementById("history-section").classList.add("hidden");
+    document.getElementById("btn-card").classList.add("bg-lime-400");
+    document.getElementById("btn-history").classList.remove("bg-lime-400");
+    document.getElementById("btn-card").classList.remove("text-slate-500");
+})
+
+
+document.getElementById("donate-now").addEventListener("click", function(){
+    donateNowButton("input-donate-amount", "card-donation-amount", "donate-title");
+})
+
+document.getElementById("donate-now-2").addEventListener("click", function(){
+    donateNowButton("input-donate-amount-2", "card-donation-amount-2", "donate-title-2");
+})
+
+document.getElementById("donate-now-3").addEventListener("click", function(){
+    donateNowButton("input-donate-amount-3", "card-donation-amount-3", "donate-title-3");
+})
+
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,57 @@
-console.log("This is from utilities js file")
+
+
+function donateNowButton (id, amount, title)
+{
+    const mainBalance = document.getElementById("main-balance").innerText;
+    const mainBalanceNumber = parseFloat(mainBalance);
+    const inputAmount = document.getElementById(id).value;
+    const inputAmountNumber = parseFloat(inputAmount);
+   
+   
+    if(!isNaN(inputAmount) && inputAmountNumber < mainBalanceNumber && inputAmountNumber > 0)
+    {
+        const newBalance = mainBalanceNumber - inputAmountNumber;
+    document.getElementById("main-balance").innerText = newBalance;
+    const cardDonationAmount = document.getElementById(amount).innerText;
+    const cardAmountNumber = parseFloat(cardDonationAmount);
+   
+    document.getElementById(amount).innerText = cardAmountNumber + inputAmountNumber;
+   
+    const donateTitle = document.getElementById(title).innerText;
+
+    // time values
+    let n = new Date()
+    const weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let i = n.getDay();
+    let week = weeks[i];
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        let j = n.getMonth();
+        let month = monthNames[j];
+        let date = n.getDate();
+        let year = n.getFullYear();
+        let hours = n.getHours();
+        let minutes = n.getMinutes();
+        let seconds = n.getSeconds();
+
+
+
+    //Adding transaction to history
+    const div = document.createElement("div");
+    div.classList.add("w-4/5", "mx-auto", "p-7", "border", "rounded-2xl");
+    div.innerHTML = `
+    <h2 class="text-xl font-extrabold pb-4">${inputAmountNumber} Taka is ${donateTitle}</h2>
+    
+    <p class="font-light text-[#111111b3]"> Date : ${week} ${month} ${date} ${year} ${hours}:${minutes}:${seconds} GMT +0600 (Bangladesh Standard Time)</p>
+    `
+    document.getElementById("history-section").appendChild(div);
+
+    document.getElementById('my_modal_1').showModal();
+    }
+    else{
+        alert("Invalid Input")
+        document.getElementById('my_modal_1').close();
+    }
+    
+
+}
